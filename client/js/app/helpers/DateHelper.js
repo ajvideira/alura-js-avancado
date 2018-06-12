e825337bfbc5a74acd0115d00ad527938ec0a9c2
Jonathan Alba Videira <jonathan.videira@gmail.com>
2018-06-12 20:10:49 -0300

    Alterando campo de data para text

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -10,14 +10,15 @@ class DateHelper {
 
     static textoParaData(texto) {
 
-        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
+        if (!/^\d{2}\/\d{2}\/\d{4}$/.test(texto)) {
             throw new Error('Deve estar no formato aaaa-mm-dd');
         }
 
         return new Date(
             ...texto
-            .split('-')
+            .split('/')
             .map((item, i) => item - i % 2)
+            .reverse()
         );
     }
 
